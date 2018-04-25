a5740e8af50a5c7a07abb17dfa7eff8db55b5e2f
Zhou Kang <dev.zhoukang@gmail.com>
2018-04-25 19:46:00 +0800

    sync

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # ZKStickyHeaderView
 微博个人页tableHeader粘连效果
-
-参考:https://github.com/freshking/StickyHeaderView 
--- ZKStickyHeaderViewDemo/ZKStickyHeaderViewDemo/Classes/ZKStickyHeaderView/ZKStickyHeaderView.m
+++ ZKStickyHeaderViewDemo/ZKStickyHeaderViewDemo/Classes/ZKStickyHeaderView/ZKStickyHeaderView.m
@@ -16,7 +16,6 @@
 @interface ZKStickyHeaderView() <UIScrollViewDelegate, MJPhotoBrowserDelegate>
 
 @property (nonatomic, strong) NSArray <NSString *> *imageNames;
-
 @property (nonatomic, assign) BOOL                 pageControlUsed;
 @property (nonatomic, strong) UIScrollView         *scrollView;
 @property (nonatomic, strong) UIPageControl        *pageControl;
@@ -29,8 +28,7 @@ static CGFloat const kPageControlBottomSpace = 15.0f;
 @implementation ZKStickyHeaderView
 
 #pragma mark - Init
-- (instancetype)initWithFrame:(CGRect)frame
-{
+- (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         [self addSubview:self.scrollView];
@@ -38,16 +36,14 @@ static CGFloat const kPageControlBottomSpace = 15.0f;
     return self;
 }
 
-+ (instancetype)headerViewWithImageNames:(NSArray<NSString *> *)imageNames initFrame:(CGRect)initFrame
-{
++ (instancetype)headerViewWithImageNames:(NSArray<NSString *> *)imageNames initFrame:(CGRect)initFrame {
     ZKStickyHeaderView *headerView = [[ZKStickyHeaderView alloc] initWithFrame:initFrame];
     headerView.imageNames = imageNames;
     return headerView;
 }
 
 #pragma mark - Timer
-- (void)handleTimer
-{
+- (void)handleTimer {
     if (_imageNames.count == 1) {
         [self removeTimer];
         return;
@@ -60,27 +56,22 @@ static CGFloat const kPageControlBottomSpace = 15.0f;
     [self.scrollView setContentOffset:offset animated:YES];
 }
 
-- (void)addTimer
-{
+- (void)addTimer {
     __weak typeof(self) weakSelf = self;
     _timer = [NSTimer zk_scheduledTimerWithTimeInterval:2 block:^{
         [weakSelf handleTimer];
     } repeates:YES];
-    
     [[NSRunLoop mainRunLoop] addTimer:_timer forMode:NSRunLoopCommonModes];
 }
 
-- (void)removeTimer
-{
+- (void)removeTimer {
     [_timer invalidate];
     self.timer = nil;
 }
 
 #pragma mark - Setter
-- (void)setImageNames:(NSArray<NSString *> *)imageNames
-{
+- (void)setImageNames:(NSArray<NSString *> *)imageNames {
     _imageNames = imageNames;
-    
     if ([_imageNames count] > 1) {
         [self addSubview:self.pageControl];
     }
@@ -88,8 +79,7 @@ static CGFloat const kPageControlBottomSpace = 15.0f;
     [self addTimer];
 }
 
-- (void)setupImageViews
-{
+- (void)setupImageViews {
     NSUInteger imageCount = _imageNames.count;
     CGFloat width = self.scrollView.bounds.size.width;
     
@@ -106,9 +96,7 @@ static CGFloat const kPageControlBottomSpace = 15.0f;
         [imageView addGestureRecognizer:[[UITapGestureRecognizer alloc]
                                          initWithTarget:self action:@selector(handleTap:)]];
         
-        
         NSString *imageNameStr = nil;
-        
         if (i == 0) {
             imageNameStr = _imageNames[imageCount-1];
             imageView.tag = imageCount-1;
@@ -137,17 +125,8 @@ static CGFloat const kPageControlBottomSpace = 15.0f;
 }
 
 #pragma mark - Private Mehods
-- (void)updateFrame:(CGRect)rect
-{
-    self.frame = rect;
-    _scrollView.frame = rect;
-    
-    float y = self.frame.size.height + _scrollView.frame.origin.y - kPageControlBottomSpace;
-    _pageControl.frame = CGRectMake(0.0f, y, self.frame.size.width, kPageControlBottomSpace);
-}
 
-- (void)updateFrameWhenScroll
-{
+- (void)updateFrameWhenScroll {
     float delta = 0.0f;
     CGRect rect = HEADER_INIT_FRAME;
     
@@ -163,33 +142,34 @@ static CGFloat const kPageControlBottomSpace = 15.0f;
     [self updateFrame:rect];
 }
 
-- (UITableView *)superTableView
-{
-   id view = [self superview];
+- (void)updateFrame:(CGRect)rect {
+    self.frame = rect;
+    _scrollView.frame = rect;
+    
+    float y = self.frame.size.height + _scrollView.frame.origin.y - kPageControlBottomSpace;
+    _pageControl.frame = CGRectMake(0.0f, y, self.frame.size.width, kPageControlBottomSpace);
+}
 
+- (UITableView *)superTableView {
+   id view = [self superview];
     while (view && ![view isKindOfClass:[UITableView class]]) {
         view = [view superview];
     }
-    
     return (UITableView *)view;
 }
 
 #pragma mark - UITapGestureRecognizer
-- (void)handleTap:(UITapGestureRecognizer *)tap
-{
+
+- (void)handleTap:(UITapGestureRecognizer *)tap {
     [self removeTimer];
-    
     if ([_delegate respondsToSelector:@selector(stickyHeaderViewDidTap:)]) {
         [_delegate stickyHeaderViewDidTap:self];
     }
-    
     [self showPhotoBrowserWithTap:tap];
 }
 
-- (void)showPhotoBrowserWithTap:(UITapGestureRecognizer *)tap
-{
+- (void)showPhotoBrowserWithTap:(UITapGestureRecognizer *)tap {
     NSInteger count = _imageNames.count;
-    
     NSMutableArray <MJPhoto *> *photos = [NSMutableArray arrayWithCapacity:count];
     for (int i = 0; i<count; i++) {
         NSString *url = _imageNames[i];
@@ -214,6 +194,7 @@ static CGFloat const kPageControlBottomSpace = 15.0f;
 }
 
 #pragma mark - ScrollView Methods
+
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
     if (_pageControlUsed) {
         return;
@@ -233,24 +214,21 @@ static CGFloat const kPageControlBottomSpace = 15.0f;
     self.pageControl.currentPage = currentPage;
 }
 
-- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
-{
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
     _pageControlUsed = YES;
     [self removeTimer];
 }
 
-
 - (void)scrollViewWillEndDragging:(UIScrollView *)scrollView
                      withVelocity:(CGPoint)velocity
-              targetContentOffset:(inout CGPoint *)targetContentOffset
-{
+              targetContentOffset:(inout CGPoint *)targetContentOffset {
     _pageControlUsed = NO;
     [self addTimer];
 }
 
 #pragma mark - Lazy Loading
-- (UIScrollView *)scrollView
-{
+
+- (UIScrollView *)scrollView {
     if (!_scrollView) {
         _scrollView = [[UIScrollView alloc] initWithFrame:self.bounds];
         [_scrollView setBackgroundColor:[UIColor whiteColor]];
@@ -264,8 +242,7 @@ static CGFloat const kPageControlBottomSpace = 15.0f;
     return _scrollView;
 }
 
-- (UIPageControl *)pageControl
-{
+- (UIPageControl *)pageControl {
     if (!_pageControl) {
         _pageControl = [[UIPageControl alloc]initWithFrame: CGRectMake(0,
                                                                        self.frame.size.height-kPageControlBottomSpace,
@@ -281,14 +258,12 @@ static CGFloat const kPageControlBottomSpace = 15.0f;
 }
 
 #pragma mark - <MJPhotoBrowserDelegate>
-- (void)photoBrowser:(MJPhotoBrowser *)photoBrowser didChangedToPageAtIndex:(NSUInteger)index
-{
-    NSLog(@"index==%zd", index);
+- (void)photoBrowser:(MJPhotoBrowser *)photoBrowser didChangedToPageAtIndex:(NSUInteger)index {
+    NSLog(@"index==%lu", (unsigned long)index);
     [_scrollView setContentOffset:CGPointMake(index*_scrollView.frame.size.width+_scrollView.frame.size.width, _scrollView.contentOffset.y) animated:NO];
 }
 
-- (void)photoBrowserDidEndShowing:(MJPhotoBrowser *)photoBrowser
-{
+- (void)photoBrowserDidEndShowing:(MJPhotoBrowser *)photoBrowser {
     [self addTimer];
 }
 
